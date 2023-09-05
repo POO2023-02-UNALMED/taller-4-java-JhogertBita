fd540d29086b30268aeda1858ca43d8bf784ddb1
JhogertBita <141960659+JhogertBita@users.noreply.github.com>
2023-09-05 09:39:09 -0500

    código asignatura

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -10,7 +10,6 @@ public class Asignatura {
 
     public Asignatura() {
         this("Sin nombre",Tipo.FUNDAMENTACION);
-        this(0);
     }
 
     public Asignatura(int codigoInterno,Tipo tipo) {
@@ -37,10 +36,6 @@ public class Asignatura {
         this.codigoExterno = codigoExterno;
         this.nombre = nombre;
         this.tipo = tipo;
-    }
-
-    public void cambiarDatos(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
     }
 
     public void cambiarDatos(int codigoExterno) {
